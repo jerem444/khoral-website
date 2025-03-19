52ea69d0bd15e4cc253b6611328fce0868fbae37
Jérémy Valayer <ext.jeremy.valayer@sncf.fr>
2025-03-19 17:32:33 +0100

    Refactorisation du CSS pour améliorer la lisibilité et l'uniformité. Mise à jour du carrousel d'images avec des ajustements de style et intégration de la pagination. Suppression de la gestion des clics pour simplifier l'interaction utilisateur.

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/swiper-bundle.css';
 import { Photo } from '@/app/portfolio/page';
 import styles from './PhotoCarousel.module.css';
 
@@ -15,31 +11,24 @@ interface PhotoCarouselProps {
 }
 
 const PhotoCarousel: React.FC<PhotoCarouselProps> = ({ images }) => {
-  const swiperRef = useRef<any>(null); // Référence pour le Swiper
-
-  const handleClick = (event: React.MouseEvent) => {
-    const { clientX } = event; // Position du clic
-    const centerX = window.innerWidth / 2; // Centre de l'écran
-
-    if (clientX < centerX) {
-      // Clic à gauche du centre
-      swiperRef.current.swiper.slidePrev(); // Faire défiler vers la gauche
-    } else {
-      // Clic à droite du centre
-      swiperRef.current.swiper.slideNext(); // Faire défiler vers la droite
-    }
-  };
-
   return (
-    <div className={styles.carouselContainer} onClick={handleClick}>
-      <Swiper ref={swiperRef}
-        modules={[Navigation]}
-        spaceBetween={50}
+    <div className={styles.carouselContainer}
+    >
+      <Swiper
+        modules={[Navigation, Pagination]}
         slidesPerView={1}
+        spaceBetween={5}
+        navigation
+        pagination
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
-            <img src={image.image} alt={`Photo ${index + 1}`} />
+            <img src={image.image} alt={`Photo ${index + 1}`} className={styles.image} />
           </SwiperSlide>
         ))}
       </Swiper>
