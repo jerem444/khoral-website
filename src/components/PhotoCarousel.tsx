b421f6ae4b84121b22732f680df004807d557402
Jérémy Valayer <ext.jeremy.valayer@sncf.fr>
2025-03-21 17:49:06 +0100

    Mise à jour du composant PhotoCarousel avec une nouvelle structure de carrousel utilisant Swiper, ajout d'effets de couverture et d'autoplay. Amélioration du style CSS pour une meilleure présentation des images et des informations associées. Intégration de boutons de navigation personnalisés et ajustements pour la réactivité sur différents écrans.

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-import 'swiper/swiper-bundle.css';
+import { Navigation, Pagination, EffectCoverflow, Autoplay } from "swiper/modules";
+import { useRef } from 'react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
 import styles from './PhotoCarousel.module.css';
 
 interface Photo {
   image: string;
   title: string;
   description: string;
-  date : Date;
+  date: Date;
 }
 
 interface PhotoCarouselProps {
@@ -17,27 +21,73 @@ interface PhotoCarouselProps {
 }
 
 const PhotoCarousel: React.FC<PhotoCarouselProps> = ({ photos }) => {
+  const prevRef = useRef<HTMLDivElement>(null);
+  const nextRef = useRef<HTMLDivElement>(null);
+  
   return (
-    <div className={styles.carouselContainer}
-    >
-      <Swiper
-        modules={[Navigation, Pagination]}
-        slidesPerView={1}
-        spaceBetween={5}
-        navigation
-        pagination
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
-      >
-        {photos.map((photo, index) => (
-          <SwiperSlide key={index}>
-            <img src={photo.image} alt={`Photo ${index + 1}`} className={styles.image} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className={styles.carouselContainer}>
+      <div className={styles.carouselWrapper}>
+        <div ref={prevRef} className={`${styles.navButton} ${styles.prevButton}`}></div>
+        <div ref={nextRef} className={`${styles.navButton} ${styles.nextButton}`}></div>
+        
+        <Swiper
+          modules={[Navigation, Pagination, EffectCoverflow, Autoplay]}
+          effect="coverflow"
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 20,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onInit={(swiper) => {
+            // @ts-ignore
+            swiper.params.navigation.prevEl = prevRef.current;
+            // @ts-ignore
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+          }}
+          loop={true}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          className={`${styles.mySwiper} ${styles.paginationContainer}`}
+          breakpoints={{
+            640: { slidesPerView: 1.3 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {photos.map((photo, index) => (
+            <SwiperSlide key={index} className={styles.swiperSlide}>
+              <div className={styles.slideContent}>
+                <div className={styles.imageContainer}>
+                  <img src={photo.image} alt={photo.title} className={styles.image} />
+                  <div className={styles.photoOverlay}>
+                    <div className={styles.photoInfo}>
+                      <h3 className={styles.photoTitle}>{photo.title}</h3>
+                      <p className={styles.photoDescription}>{photo.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
